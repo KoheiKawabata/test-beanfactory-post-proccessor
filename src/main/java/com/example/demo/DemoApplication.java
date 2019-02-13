c027ae756860092e6d38cbcb022af86e32c77c1c
Kohei Kawabata <kohei.kawabata@mac.com>
2019-02-13 11:48:00 +0900

    Add sample.

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+@Slf4j
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public EnvironmentsConfigurer environmanetsConfigurer(){
+        return new EnvironmentsConfigurer();
+    }
+
+    public class EnvironmentsConfigurer implements ApplicationContextAware, BeanFactoryPostProcessor{
+
+        @Autowired
+        ApplicationContext applicationContext;
+
+        @Override
+        public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory)
+                throws BeansException {
+            Environment environment = applicationContext.getEnvironment();
+            log.info("Setting property here from enviroment variables.");
+        }
+
+        @Override
+        public void setApplicationContext(ApplicationContext applicationContext)
+                throws BeansException {
+            this.applicationContext = applicationContext;
+        }
+    }
+
+}
+
